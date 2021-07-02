bf3c02895975f18845476fa8cb4b9e59ecc5deec
Sharon Shih <en-ping.shih@sjsu.edu>
2021-07-02 15:41:53 -0700

    fix ui typo

--- app/backend/client/src/components/home/navbar_home.js
+++ app/backend/client/src/components/home/navbar_home.js
@@ -16,7 +16,7 @@ class NavBar extends Component {
           <div className="col-md-10 col-lg-8 col-xl-7 mx-auto search">
             <form>
               <div className="form-row">
-                <div className="col-12 col-md-9 mb-2 mb-md-0"><input className="form-control form-control-lg" type="search" placeholder="Search Stock Here..." /></div>
+                <div className="col-12 col-md-9 mb-2 mb-md-0"><input className="form-control form-control-lg" type="search" placeholder="Search StudyPool Here..." /></div>
                 <div className="col-12 col-md-3"><button className="btn btn-block btn-lg icon-search" type="search" /></div>
               </div>
             </form>
